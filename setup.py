1a858c3d57497f5fd79b363952333bc836e8786d
chrissy <chrissy@7abc7aa1-7e52-db11-9331-00508bd72530>
2010-10-26 14:41:06 +0000

    checking in the initial nyu theme refs #2
    git-svn-id: https://svn.sixfeetup.com/svn/private/nyu/isaw.theme/trunk@25861 7abc7aa1-7e52-db11-9331-00508bd72530

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1dev'
+
+setup(name='isaw.theme',
+      version=version,
+      description="An installable theme for Plone 4",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='web zope plone theme',
+      author='Six Feet Up, Inc.',
+      author_email='info@sixfeetup.com',
+      url='http://www.sixfeetup.com',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['isaw'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+          'sixfeetup.utils',
+      ],
+      entry_points="""
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
+
