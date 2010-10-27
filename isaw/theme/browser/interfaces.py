bb0eb0afdf483e7c1efaebeb95d65f7b310efdbc
chrissy <chrissy@7abc7aa1-7e52-db11-9331-00508bd72530>
2010-10-27 13:38:04 +0000

    moved the main navigation to a left column and styled it refs #5
    git-svn-id: https://svn.sixfeetup.com/svn/private/nyu/isaw.theme/trunk@25897 7abc7aa1-7e52-db11-9331-00508bd72530

--- isaw/theme/browser/interfaces.py
+++ isaw/theme/browser/interfaces.py
@@ -1,5 +1,11 @@
 from plone.theme.interfaces import IDefaultPloneLayer
+from zope.viewlet.interfaces import IViewletManager
 
 class IThemeSpecific(IDefaultPloneLayer):
     """Marker interface that defines a Zope 3 browser layer.
+    """
+    
+class ILeftCol(IViewletManager):
+    """A viewlet manager that sits in the left most column of the page
+       (an addition to the main template)
     """
