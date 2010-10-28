3fcf170dd72349520b77a64c9bf75109fdf6de30
chrissy <chrissy@7abc7aa1-7e52-db11-9331-00508bd72530>
2010-10-28 20:40:26 +0000

    enabled image captions and styled them refs #22
    git-svn-id: https://svn.sixfeetup.com/svn/private/nyu/isaw.theme/trunk@25966 7abc7aa1-7e52-db11-9331-00508bd72530

--- isaw/theme/skins/isaw_theme_styles/isaw.js
+++ isaw/theme/skins/isaw_theme_styles/isaw.js
@@ -3,6 +3,18 @@ jq(document).ready(function(){
    jq("li .submenu").hover(
      function () { jq(this).parent().addClass("hover"); }, 
      function () { jq(this).parent().removeClass("hover"); }
-   );
-   
+   );   
+});
+
+
+jq(window).load(function(){
+    // set with on image <dl>s to be set at the width of the image
+    // if there is a caption, the width gets pushed out
+    capnum = jq("dl.captioned").length;
+    console.log(capnum);
+
+    for(i = 0; i < capnum; i++) {
+         imgWidth = jq("dl.captioned img").eq(i).width();
+         jq("dl.captioned").css("width",imgWidth);
+    }
 });
